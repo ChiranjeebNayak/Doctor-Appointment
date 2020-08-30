7d22a43c39d7ab9c946c61d22d7747f752c5fa66
Chiranjeeb <ChiranjeebNayak.37@gmail.com>
2020-08-31 02:34:41 +0530

    update some code

--- JS/doctor.js
+++ JS/doctor.js
@@ -1,3 +1,23 @@
 function back(){
     window.history.go(-1);
 }
+var citiesByState = {
+    Odisha: ["Bhubaneswar","Puri","Cuttack","Bhadrak","Balasore","Balangir","Khordha","Keonjhar","Nabarangpur","Others"],
+    Maharashtra: ["Mumbai","Pune","Nagpur"],
+    Kerala: ["kochi","Kanpur"],
+    }
+    function makeSubmenu(value) {
+    if(value.length==0) 
+    document.getElementById("citySelect").innerHTML = "<option></option>";
+    else {
+    var citiesOptions = "";
+    for(cityId in citiesByState[value]) {
+    citiesOptions+="<option>"+citiesByState[value][cityId]+"</option>";
+    }
+    document.getElementById("citySelect").innerHTML = citiesOptions;
+    }
+    }
+    function resetSelection() {
+        document.getElementById("countrySelect").selectedIndex = 0;
+        document.getElementById("citySelect").selectedIndex = 0;
+        }
