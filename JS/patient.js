7526ca2a3843e956ba1ab70d26c3e5349093e637
Chiranjeeb <ChiranjeebNayak.37@gmail.com>
2020-08-31 21:47:38 +0530

    pregister update

--- JS/patient.js
+++ JS/patient.js
@@ -2,17 +2,17 @@ function back(){
     window.history.go(-1);
 }
 function register(){
-    var name= document.getElementById("name");
+    var name= document.getElementById("name").value;
     console.log(name);
-    var dob= document.getElementById("dob");
+    var dob= document.getElementById("dob").value;
     console.log(dob);
-    var phone= document.getElementById("phone");
+    var phone= document.getElementById("phone").value;
     console.log(dob);
-    var email= document.getElementById("email");
+    var email= document.getElementById("email").value;
     console.log(email);
-    var pwd=document.getElementById("password");
+    var pwd=document.getElementById("password").value;
     console.log(pwd);
-    var repwd= document.getElementById("password")
+    var repwd= document.getElementById("password").value;
     console.log(repwd);
     return true;
 }
