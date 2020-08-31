20fdaf3adf45914aaaa08737854eef2aa9e02b75
Chiranjeeb <ChiranjeebNayak.37@gmail.com>
2020-08-31 21:30:09 +0530

    update patient register

--- JS/patient.js
+++ JS/patient.js
@@ -1,3 +1,18 @@
 function back(){
     window.history.go(-1);
 }
+function register(){
+    var name= document.getElementById("name");
+    console.log(name);
+    var dob= document.getElementById("dob");
+    console.log(dob);
+    var phone= document.getElementById("phone");
+    console.log(dob);
+    var email= document.getElementById("email");
+    console.log(email);
+    var pwd=document.getElementById("password");
+    console.log(pwd);
+    var repwd= document.getElementById("password")
+    console.log(repwd);
+    return true;
+}
